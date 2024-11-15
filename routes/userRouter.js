de1946c344eacd60fca7f0b013812ab55b11ec0a
TharinduDilshanEkanayake <ekanayakeemtd.21@uom.lk>
2024-11-15 22:50:43 +0530

    jwt

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 const { createUser } = require('../controllers/userController');
+const { loginUser } = require('../controllers/authController')
 
 router.post('/createuser', createUser)
+router.post('/login',loginUser)
 
 module.exports = router;
